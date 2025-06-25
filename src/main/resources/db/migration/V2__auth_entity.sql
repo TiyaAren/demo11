829704348c865b3bdebbdede98953c2725e1365b
Pc-7 <tiyaaren62@gmail.com>
2025-06-25 20:41:51 +0300

    исравление таблиц

--- src/main/resources/db/migration/V2__auth_entity.sql
+++ src/main/resources/db/migration/V2__auth_entity.sql
@@ -1,13 +1,13 @@
-CREATE TABLE IF NOT EXISTS users_schema.t_roles(
-    id SERIAL PRIMARY KEY,
+CREATE TABLE  IF NOT EXISTS users_schema.t_roles(
+    id SERIAL PRIMARY KEY ,
     c_name VARCHAR(50) UNIQUE NOT NULL
 );
-CREATE TABLE IF NOT EXISTS users_schema.t_credentials(
-    id SERIAL PRIMARY KEY,
-    c_username VARCHAR(50), UNIQUE NOT NULL,
-    c_password VARCHAR(255) NOT NULL,
-    c_user_id INTEGER NOT NULL,
-    c_role_id INTEGER NOT NULL,
-    FOREIGN KEY (c_user_id) REFERENCES users_schema.t_users(id)
+CREATE TABLE  IF NOT EXISTS users_schema.t_credentials(
+    id SERIAL PRIMARY KEY ,
+    c_username VARCHAR(50) UNIQUE  NOT NULL,
+    c_password VARCHAR(255) NOT NULL ,
+    c_user_id INTEGER NOT NULL ,
+    c_role_id INTEGER NOT NULL ,
+    FOREIGN KEY (c_user_id) REFERENCES users_schema.t_users(id),
     FOREIGN KEY (c_role_id) REFERENCES users_schema.t_roles(id)
 );
